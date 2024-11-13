14b3e2d4a63c751d8c180e8bb5f9aacdc4b3be56
sarath <sarathmail@gmail.com>
2024-11-13 12:54:47 +0530

    Update main.py
    test endpoint added

--- api/main.py
+++ api/main.py
@@ -8,4 +8,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World from AWS Lambda"}
 
+
+@app.get("/test")
+async def my_test():
+    return {"message": "test from AWS Lambda"}
+
+
 handler = Mangum(app=app)
