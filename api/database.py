485438d7df3cf61a39eb8ab6c369b0b55dcd2fc3
sarath <sarathmail@gmail.com>
2024-11-13 18:09:03 +0530

    database url pointed to RDS
    SQLAlchemy database url pointed to RDS

--- api/database.py
+++ api/database.py
@@ -6,7 +6,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://@DESKTOP-4MOISVD/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+#SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://@DESKTOP-4MOISVD/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://admin:srikar123%23@firstdb.cro480eaysw6.eu-north-1.rds.amazonaws.com/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
