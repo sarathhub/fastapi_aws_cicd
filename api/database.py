b0329412eacb45dee0c4d4fff1ce97f772967ce4
sarath <sarathmail@gmail.com>
2024-11-13 19:11:49 +0530

    added connect timeout
    Added connect timeout

--- api/database.py
+++ api/database.py
@@ -8,8 +8,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://@DESKTOP-4MOISVD/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
 
-SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://admin:srikar123%23@firstdb.cro480eaysw6.eu-north-1.rds.amazonaws.com/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server"
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://admin:srikar123%23@firstdb.cro480eaysw6.eu-north-1.rds.amazonaws.com/COACH_LMS?driver=ODBC+Driver+17+for+SQL+Server&Connect Timeout=30"
 
+#SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://username:password@server/database?driver=ODBC+Driver+17+for+SQL+Server&Connect Timeout=30"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
